6f592389b3f785a497a8a306e998b49dd95a8427
Nathan Bergey <nathan.bergey@gmail.com>
2016-07-28 15:58:24 -0600

    Stub for loading TLE from strings

--- src/lib.rs
+++ src/lib.rs
@@ -25,10 +25,10 @@ pub struct TLE {
     pub sat_number: u32,
 
     /// Classification (U=Unclassified)
-    pub classification: u8,
+    pub classification: char,
 
     /// International Designator
-    pub int_designator: String,
+    pub int_designator: &'static str,
 
     /// Epoch Year
     pub epoch_year: u16,
@@ -52,7 +52,7 @@ pub struct TLE {
     pub i: f64,
 
     /// Right ascension of the ascending node
-    pub RA: f64,
+    pub raan: f64,
 
     /// Eccentricity
     pub e: f64,
@@ -68,6 +68,50 @@ pub struct TLE {
 
     /// Revolution number at epoch (revolutions)
     pub revolution_number: u32,
+}
+
+
+impl TLE {
+
+    /// Read a TLE from Strings
+    pub fn load_from_str(self, line1: String, line2: String, line3: String) -> TLE {
+
+        let sat_number = 0;
+        let classification = 'U';
+        let int_designator = "";
+        let epoch_year = 0;
+        let epoch_day = 0.0;
+        let first_mean_motion = 0.0;
+        let second_mean_motion = 0.0;
+        let bstar = 0.0;
+        let tle_version = 0;
+        let i = 0.0;
+        let raan = 0.0;
+        let e = 0.0;
+        let omega = 0.0;
+        let mean_anomaly = 0.0;
+        let mean_motion = 0.0;
+        let revolution_number = 0;
+
+        TLE {
+            sat_number: sat_number,
+            classification: classification,
+            int_designator: int_designator,
+            epoch_year: epoch_year,
+            epoch_day: epoch_day,
+            first_mean_motion: first_mean_motion,
+            second_mean_motion: second_mean_motion,
+            bstar: bstar,
+            tle_version: tle_version,
+            i: i,
+            raan: raan,
+            e: e,
+            omega: omega,
+            mean_anomaly: mean_anomaly,
+            mean_motion: mean_motion,
+            revolution_number: revolution_number,
+        }
+    }
 }
 
 
