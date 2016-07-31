a6fc58535274ad3868de8f16f312d91df5406e5a
Nathan Bergey <nathan.bergey@gmail.com>
2016-07-31 13:43:14 -0600

    Some docs for J2000 coordinates

--- src/coordinates.rs
+++ src/coordinates.rs
@@ -9,16 +9,24 @@
 
 #![allow(non_snake_case)]
 
-/// ## J2000 ECI
+/// ## J2000
+///
+/// A commonly used ECI (**E**arth **C**entered **I**nertial) frame is defined with the Earth's Mean Equator
+/// and Equinox at 12:00 Terrestrial Time on 1 January 2000. The x-axis is
+/// aligned with the mean equinox. The z-axis is aligned with the Earth's
+/// spin axis or celestial North Pole. The y-axis is rotated by 90° East
+/// about the celestial equator.
+///
+/// See [Earth Centered Inertial Coordinates](https://en.wikipedia.org/wiki/Earth-centered_inertial)
 #[derive(Debug, PartialEq, PartialOrd)]
 pub struct J2000 {
 
-    /// X
+    /// $X$
     pub X: f64,
 
-    /// Y
+    /// $Y$
     pub Y: f64,
 
-    /// Z
+    /// $Z$
     pub Z: f64,
 }
